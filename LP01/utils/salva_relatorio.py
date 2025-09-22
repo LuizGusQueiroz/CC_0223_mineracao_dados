04dff11b101cc600bcb5c41e2b45fd35eb5b0149
Luiz Gustavo <luizgusqueiroz@gmail.com>
2025-09-21 21:27:48 -0300

    feat: add função para salvar relatório em txt e json

--- LP01/utils/salva_relatorio.py
+++ LP01/utils/salva_relatorio.py
@@ -1,5 +1,14 @@
-def salva_relatorio(relatorio: str, nome: str = 'relatorio_eda') -> None:
-    file_name = f'{nome}.txt'
+from typing import Dict
+import json
+
+
+def salva_relatorio_txt(relatorio: str, rel_path: str='relatorio_eda') -> None:
+    file_name = f'{rel_path}.txt'
     with open(file_name, 'w') as file:
         file.write(relatorio)
 
+
+def salva_relatorio_json(relatorio: Dict, rel_path: str='relatorio_eda') -> None:
+    file_name = f'{rel_path}.json'
+    with open(file_name, 'w', encoding='utf-8') as file:
+        json.dump(relatorio, file, indent=4, ensure_ascii=False, default=str)
